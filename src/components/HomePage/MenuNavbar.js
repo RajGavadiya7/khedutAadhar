c7b855e9cd0aab6536d87c260567722769e9e2e0
RajGavadiya7 <rajpatel341233@gmail.com>
2023-06-18 09:37:01 +0530

    random commit

--- src/components/HomePage/MenuNavbar.js
+++ src/components/HomePage/MenuNavbar.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
-// import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import { Burger } from '@mantine/core';
 
